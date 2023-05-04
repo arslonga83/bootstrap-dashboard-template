fcab45a17b566a29f23f5d96d9b249bc5b164759
arslonga83 <jlwood83@gmail.com>
2023-05-04 15:50:38 -0700

    add side nav

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,11 @@ import '../scss/styles.scss'
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+
+const btn = document.querySelector('.btn-slide');
+
+btn.addEventListener('click', () => {
+  document.querySelector('.side-nav').classList.toggle('slide')
+  document.querySelector('.open').classList.toggle('d-none')
+  document.querySelector('.close').classList.toggle('d-none')
+})
